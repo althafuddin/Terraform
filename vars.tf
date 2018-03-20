fea9b6f4f19278f5642e8a6ee020b5a23c7bd188
Althaf <althafuddin@outlook.com>
2018-03-20 12:27:11 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,8 @@
+## Create tfvars or export the Aws credentials##
+
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-  default = "us-west-1"
+  default = "<Your defaulr Region>"
 }
